c30ec0652c8b4f19a0e4a22ea74b559ec59894e5
kimsehwan <freezkim@gmail.com>
2021-03-31 17:57:22 +0900

    main page study done.

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ searchInputEl.addEventListener('blur', function() {
   searchInputEl.setAttribute('placeholder', '');
 });
 
-const badgeEL = document.querySelector('header .badges');
+const badgeEl = document.querySelector('header .badges');
 
 window.addEventListener('scroll', _.throttle(function ()
 {
@@ -26,18 +26,135 @@ window.addEventListener('scroll', _.throttle(function ()
   {
     //todo: hidden badge
     //gsap.to(element, duration, option);
-    gsap.to(badgeEL, .3, {
+    gsap.to(badgeEl, .3, {
       opacity: 0, 
       display: 'none'
     });
+
+    //todo: show Button.
+    gsap.to(toTopEl, .2, {
+      x: 0
+    });
+
   }
   else
   {
     //todo: show badge.
-    gsap.to(badgeEL, .3, {
+    gsap.to(badgeEl, .3, {
       opacity: 1,     
       display: 'block' 
     });
+
+    //todo: hide Button.
+    gsap.to(toTopEl, .2, {
+      x: 100
+    });
+
   }
 }, 300));
 
+const toTopEl = document.querySelector('#to-top');
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, .7, {
+    scrollTo: 0
+  });
+})
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index)*0.5,
+    opacity: 1
+  });
+});
+
+//new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction:'vertical',
+  autoplay: true,
+  loop: true
+});
+
+
+new Swiper('.promotion .swiper-container', {    
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination:  {
+    el: '.promotion .swiper-pagination',
+    clickable:true,
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
+new Swiper('.awards .swiper-container', {
+  slidesPerView: 5, 
+  spaceBetween: 30,  
+  autoplay: true,
+  loop: true,
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next'
+  }
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function() {
+  isHidePromotion = !isHidePromotion
+  if(isHidePromotion)
+  {
+    promotionEl.classList.add('hide');
+  }
+  else
+  {
+    promotionEl.classList.remove('hide');
+  }
+})
+
+
+function random(min, max) {
+  return parseFloat((Math.random() * (max - min) + min));
+}
+
+function floatingObject(selector, delay, size) {
+  gsap.to(
+    selector, 
+    random(1.5, 2.5), 
+    {
+      y: size,
+      repeat: -1,
+      yoyo: true,
+      ease: Power1.easeInOut,
+      delay: random(0, delay)
+    }
+  );
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+const spyEls = document.querySelectorAll('section.scroll-spy')
+spyEls.forEach(  function (spyEl)  {
+    new ScrollMagic
+    .Scene({
+      triggerElement: spyEl,
+      triggerHook: .8,
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+   }
+);
+
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
